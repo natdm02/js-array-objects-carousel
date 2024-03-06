fb80507fdfd35ac6d86f7d642b1fd40cf5627e10
natdm02 <natalidimarino2002>
2024-03-06 15:48:13 +0100

    [add] html + inizio js

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const images = [
+  {
+      image: 'img/01.webp',
+      title: 'Marvel\'s Spiderman Miles Morale',
+      text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores, quisquam?',
+  }, {
+      image: 'img/02.webp',
+      title: 'Ratchet & Clank: Rift Apart',
+      text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores, quisquam?',
+  }, {
+      image: 'img/03.webp',
+      title: 'Fortnite',
+      text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores, quisquam?",
+  }, {
+      image: 'img/04.webp',
+      title: 'Stray',
+      text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores, quisquam?',
+  }, {
+      image: 'img/05.webp',
+      title: "Marvel's Avengers",
+      text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores, quisquam?',
+  }
+];
