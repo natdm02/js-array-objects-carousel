1a4ede7424a21238a7c19906cd215950028a9b69
natdm02 <natalidimarino2002>
2024-03-06 16:28:56 +0100

    [add] js

--- js/script.js
+++ js/script.js
@@ -21,3 +21,41 @@ const images = [
       text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores, quisquam?',
   }
 ];
+
+
+const mainContainerEl = document.getElementById("main-image");
+const mainImageTextEl = document.getElementById("main-image-text");
+const sliderEl = document.getElementById("slider-container");
+
+let index = 0;
+let counter = 0;
+
+for (const image of images){
+
+  const imgMain = document.createElement("img");
+  const imgSlider = document.createElement("img");
+
+  const divEl = document.createElement("div");
+  const imgTitle = document.createElement("h2");
+  const imgText = document.createElement("p");
+
+  imgMain.src = image.image;
+  imgSlider.src = image.image;
+
+  imgTitle.innerHTML = image.title;
+  imgText.innerHTML = image.text;
+
+  if(index > 0){
+    imgMain.classList.add("hide");
+    imgTitle.classList.add("hide");
+    imgText.classList.add("hide");
+
+  }
+
+ mainContainerEl.append(imgMain);
+ sliderEl.append(imgSlider);
+ mainImageTextEl.append(divEl);
+ divEl.append(imgTitle, imgText);
+ index++;
+}
+
